cded71ef501187c4fe71eb1bd4d35a6f340e6276
a4z <harald.achitz@gmail.com>
2024-05-08 11:57:39 +0200

    Update license

--- run_clang_tidy.sh
+++ run_clang_tidy.sh
@@ -3,5 +3,5 @@
 for file in $(git ls-files | grep -E '^(include|src)/.*\.(c|cpp|h|hpp)$')
 do
     echo 'Checking '$file
-    clang-tidy --extra-arg=-std=c++$1 -p $2 --header-filter='^$' $file || exit 255
+    clang-tidy -extra-arg=-Wno-unknown-warning-option --extra-arg=-std=c++$1 -p $2 --header-filter='^$' $file || exit 255
 done
